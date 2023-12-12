b3a5d607c405f3c9412e881e0900324503dd8c87
jdr14 <jdr14@vt.edu>
2023-12-12 13:38:12 -0500

    Initial Commit

---
+++ LinkedList/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LinkedList
+//
+//  Created by Joseph Rodgers on 12/12/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
